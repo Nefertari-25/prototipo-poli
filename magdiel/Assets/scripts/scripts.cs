c2c0d2d59039ff8181404e4456d60f974d49cb04
becker-aks <115589364+becker-aks@users.noreply.github.com>
2023-03-04 21:59:37 -0600

    animaciones y paisaje
    Acá hice las animaciones de movimiento, e hice un pequeño prototipo del paisaje, luego lo mejoraré

---
+++ magdiel/Assets/scripts/scripts.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scripts : MonoBehaviour
+{
+    public float correr = 1;
+
+    public float caminar = 2;
+
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate ()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(caminar, rb2D.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-caminar, rb2D.velocity.y);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+        }
+        
+            
+            
+        
+    }
+ }
